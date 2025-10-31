78e931da245c1fea571535998b20dc3c54e299d9
Robert <ackleyrc@gmail.com>
2025-10-30 23:59:36 -0700

    Pass engine config in via game config

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,19 +1,14 @@
 #include <SparticleEngine.h>
 #include <EngineConfig.h>
 #include "MazeEaterGame.h"
+#include "GameConfig.h"
 
 int main( int argc, char* argv[] )
 {
+	GameConfig gameConfig;
 	MazeEaterGame game;
 
-	EngineConfig config;
-	config.width = 448;
-	config.height = 512;
-	config.windowTitle = "Maze Eater Game";
-	config.backgroundColor = Color( 0.06f, 0.06f, 0.12f, 1.0f );
-	config.resizable = true;
-
-	SparticleEngine engine( config, &game );
+	SparticleEngine engine( gameConfig.engineConfig, &game );
 
 	engine.run();
 
