66b3231b4533add8bad8464f6a4efd29fc411bf9
Robert <ackleyrc@gmail.com>
2025-10-30 23:59:36 -0700

    Set MazeEaterGame background color

--- Game/main.cpp
+++ Game/main.cpp
@@ -10,6 +10,7 @@ int main( int argc, char* argv[] )
 	config.width = 448;
 	config.height = 512;
 	config.windowTitle = "Maze Eater Game";
+	config.backgroundColor = Color( 0.06f, 0.06f, 0.12f, 1.0f );
 	config.resizable = true;
 
 	SparticleEngine engine( config, &game );
